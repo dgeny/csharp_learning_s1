7a692fd5d0be8ceb956898e4b2f33cdb414986f5
Eugene N <kan1fol@ya.ru>
2023-01-14 20:52:33 +0200

    Task 7 added

---
+++ seminar_1/task_7/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, выводящая последнюю цифру трехзначного числа
+
+Console.WriteLine("Программа, выводящая последнюю цифру трехзначного числа");
+System.Console.WriteLine();
+
+int hundered_number = 0;
+
+do {
+    System.Console.Write("Введите целое трехзначное число:\t");
+}
+while (!Int32.TryParse(
+    System.Console.ReadLine(), out hundered_number)
+    || (100 > hundered_number) 
+    || (hundered_number > 999));
+
+System.Console.WriteLine(hundered_number % 10);

b49b58ae5759e6ad6be3301e3a7c34b3a06e6129
Eugene N <kan1fol@ya.ru>
2023-01-14 21:55:17 +0200

    Homework: task number 6  complete

---
+++ seminar_1/task_6/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+Console.WriteLine("Проверка числа на четность");
+System.Console.WriteLine();
+
+System.Console.Write("Введите число: ");
+int number = int.Parse(System.Console.ReadLine());
+
+System.Console.Write("Это число ");
+if(number%2 == 0){
+    System.Console.Write(" четное");
+}
+else {
+    System.Console.Write(" нечетное");
+}
+

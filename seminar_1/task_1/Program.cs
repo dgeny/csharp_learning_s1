d922c3657d56d5efd3e0dab88536671c1400e10f
Eugene N <kan1fol@ya.ru>
2023-01-14 18:41:26 +0200

    Example 1 added

---
+++ seminar_1/task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello!");
+
+int number;
+
+do
+{
+    number = 0;
+    Console.Write("Enter integer number: ");    
+} while (!Int32.TryParse(Console.ReadLine(),out number));
+
+int squre = number*number;
+
+System.Console.WriteLine($"Квадрат числа: {squre}");

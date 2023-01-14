d56a80b35057385aa454b179bbc03d141c5e0ffc
Eugene N <kan1fol@ya.ru>
2023-01-14 21:13:27 +0200

    Homework: task number2  complete

--- seminar_1/task_1/Program.cs
+++ seminar_1/task_1/Program.cs
@@ -12,3 +12,22 @@ do
 int squre = number*number;
 
 System.Console.WriteLine($"Квадрат числа: {squre}");
+
+
+// Задача на проверку того, является ли первое число квадратом второго
+System.Console.WriteLine("Задача на проверку того, является ли первое число квадратом второго");
+System.Console.WriteLine();
+
+int first_number=0, two_number = 0;
+System.Console.WriteLine("Enter first number: ");
+first_number = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Enter next number: ");
+two_number = int.Parse(Console.ReadLine());
+
+if (first_number == two_number*two_number)
+{
+    System.Console.WriteLine($"Число {first_number} является квадратом числа {two_number}");
+}
+else{
+    System.Console.WriteLine($"Число {first_number} не является квадратом числа {two_number}");
+}
--- seminar_1/task_2/Program.cs
+++ seminar_1/task_2/Program.cs
@@ -1,17 +1,27 @@
-﻿// Задача на проверку того, является ли первое число квадратом второго
-System.Console.WriteLine("Задача на проверку того, является ли первое число квадратом второго");
-System.Console.WriteLine();
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
 
-int first_number=0, two_number = 0;
-System.Console.WriteLine("Enter first number: ");
-first_number = int.Parse(Console.ReadLine());
-System.Console.WriteLine("Enter next number: ");
-two_number = int.Parse(Console.ReadLine());
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
 
-if (first_number == two_number*two_number)
-{
-    System.Console.WriteLine($"Число {first_number} является квадратом числа {two_number}");
+System.Console.WriteLine("Программа, выводящая какое число большее");
+System.Console.Write("Введите первое число: ");
+int first_number = int.Parse(System.Console.ReadLine());
+
+System.Console.Write("Введите второе число: ");
+int two_number = int.Parse(System.Console.ReadLine());
+
+// Решение 1.
+if (first_number > two_number){
+    System.Console.WriteLine($"Первое введенное число ({first_number}) больше второго ({two_number})\nmax = {first_number}");
 }
-else{
-    System.Console.WriteLine($"Число {first_number} не является квадратом числа {two_number}");
+else if(first_number < two_number){
+    System.Console.WriteLine($"Второе введенное число ({two_number}) больше первого ({first_number})\nmax = {two_number}");
 }
+else {
+    System.Console.WriteLine("Введенные числа одинаковы");
+}
+
+// Решение 2.
+System.Console.WriteLine($"max = {Int32.Max(first_number, two_number)}");

93e59e70ac11ccae3c6a92756bf150a889ebd59c
Eugene N <kan1fol@ya.ru>
2023-01-14 20:17:45 +0200

    Task 5 added

---
+++ seminar_1/task_5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, которая выдает все числа в промежутке от -N до N, где N - число, вводимое пользователем
+
+Console.WriteLine("Программа, которая выдает все числа в промежутке от -N до N, где N - число, вводимое пользователем");
+System.Console.WriteLine();
+
+int counter = 0;
+
+do {
+    System.Console.Write("Введите целое число:\t");
+}
+while (!Int32.TryParse(System.Console.ReadLine(), out counter));
+
+for (int i=-counter; i < counter + 1; i++){
+    System.Console.Write($"{i} ");
+}
+System.Console.WriteLine();

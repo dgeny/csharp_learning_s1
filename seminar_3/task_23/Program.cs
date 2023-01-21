7224472634f0621150ef6330e2b14a3ef0a44a4f
Eugene N <kan1fol@ya.ru>
2023-01-21 21:09:34 +0200

    Homework3: task 23 added

---
+++ seminar_3/task_23/Program.cs
@@ -0,0 +1,23 @@
+﻿
+/* 
+Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+ */
+
+int number;
+do
+{
+    number = -1;
+    System.Console.Write("Введите неотрицательное число: ");
+}
+while (!(int.TryParse(Console.ReadLine(), out number) && number > 0));
+
+for (int i=1; i < number+1; i++)
+{
+    System.Console.Write($"{Math.Pow(i,3)}, ");
+}
+System.Console.WriteLine();

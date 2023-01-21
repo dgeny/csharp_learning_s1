c2f00cbae4f9db2d0ac1fc498f893ddb48a87eb3
Eugene N <kan1fol@ya.ru>
2023-01-21 13:58:23 +0200

    Homework2: tasks 10 solution added

---
+++ seminar_2/task_10/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+// Вариант 1.
+int number = 0;
+do
+{
+    System.Console.Write("Введите трехзначное число:");
+}
+while (!
+        (int.TryParse(Console.ReadLine(), out number) &&
+        (number > 99) && 
+        (number < 1000))
+);
+
+System.Console.WriteLine(number  % 100 / 10 % 10); 
+
+// Вариант 2.
+string s_number = "";
+while (s_number.Length != 3 && !int.TryParse(s_number, out number))
+{
+    System.Console.Write("Введите трехзначное число:");
+    s_number = Console.ReadLine();
+}
+
+
+System.Console.WriteLine(s_number[1]);

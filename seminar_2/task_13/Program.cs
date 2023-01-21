822cfdb9025a1f9e7da4f436f712d82521ea80ba
Eugene N <kan1fol@ya.ru>
2023-01-21 14:23:03 +0200

    Homework2: task 13 solution added

---
+++ seminar_2/task_13/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Int128 number = 0;
+do
+{
+    System.Console.Write("Введите произвольное число, начиная с 100:");
+}
+while (!
+        (Int128.TryParse(Console.ReadLine(), out number) &&
+        (number > 99))
+);
+
+while(number  > 1000)
+{
+        number = number / 10;
+}
+System.Console.WriteLine($"Третья  цифра числа - {number % 10}");

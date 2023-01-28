1dd5525a4329b867fa840f9861aab344ba96acb7
Eugene N <kan1fol@ya.ru>
2023-01-28 11:33:18 +0200

    Homework4: task 29 added

---
+++ seminar_4/task_29/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 29: 
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+
+
+void FillArray(int[] numbers){
+    Random rnd = new Random();
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        // В условии не четко сформулированы границы задаваемых чисел
+        // поэтому примем их от 0 до 100
+        numbers[i] = rnd.Next(0,100);
+    }
+}
+
+void PrintArray(int[] numbers){
+    /*
+    System.Console.Write("[");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        System.Console.Write($"{numbers[i]}, ");
+    }
+    System.Console.Write("]");*/
+    System.Console.WriteLine("[ "+ string.Join(", ", numbers) + " ]");
+}
+
+int[] numbers = new int[8];
+FillArray(numbers);
+PrintArray(numbers);
+System.Console.WriteLine();

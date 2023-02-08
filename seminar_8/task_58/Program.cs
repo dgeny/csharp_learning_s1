6e2e4a5394142dabb9a3b62d3363668812313647
Eugene N <kan1fol@ya.ru>
2023-02-09 01:46:52 +0200

    Homework8: task 58 added

--- seminar_8/task_58/Program.cs
+++ seminar_8/task_58/Program.cs
@@ -1,1 +1,85 @@
-﻿
+﻿/* Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18 */
+
+
+int[,] FillArray(int Row, int Col, int leftRange, int rightRange)
+{
+    int[,] numbers = new int[Row, Col];
+    Random rnd = new Random();
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            numbers[i, j] = rnd.Next(leftRange, rightRange + 1);
+        }
+    }
+    return numbers;
+}
+
+void PrintArray(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < numbers.GetLength(1); j++)
+            System.Console.Write(numbers[i, j] + "\t");
+
+        System.Console.WriteLine();
+    }
+}
+
+int InputNumber(string message)
+{
+    int number = -1;
+    while (true)
+    {
+        System.Console.Write(message + ": ");
+        if (!(int.TryParse(Console.ReadLine(), out number)) && number > 0)
+        {
+            System.Console.WriteLine("Введено некорректное число, попробуйте еще раз.");
+        }
+        else break;
+    }
+    return number;
+}
+
+int[,] MultiplyMatrix(int[,] matrix1, int[,] matrix2)
+{
+    int[,] result = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix1.GetLength(1); k++)
+            {
+                result[i, j] += matrix1[i, k] * matrix2[k, j];
+            }
+        }
+    }
+    return result;
+}
+
+
+int M = InputNumber("Введите количество строк первой матрицы");
+int N = InputNumber("Введите количество столбцов первой матрицы");
+int Z = InputNumber("Введите количество столбцов второй матрицы");
+
+int leftRange = 0, rightRange = 9;
+int[,] matrix1 = FillArray(M, N, leftRange, rightRange);
+int[,] matrix2 = FillArray(N, Z, leftRange, rightRange);
+
+
+int[,] result = MultiplyMatrix(matrix1, matrix2);
+System.Console.WriteLine("Матрица 1:");
+PrintArray(matrix1);
+System.Console.WriteLine("Матрица 2:");
+PrintArray(matrix2);
+System.Console.WriteLine("Произведение матриц:");
+PrintArray(result);
+System.Console.WriteLine();

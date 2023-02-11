dd403df2a9ccea0d9099139d5a9b2826e31f2b86
Eugene N <kan1fol@ya.ru>
2023-02-12 01:57:45 +0200

    Homework9: tasks 66 added

---
+++ seminar_9/task_66/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных 
+элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+int InputNumber(string message)
+{
+    int number = -1;
+    while (true)
+    {
+        System.Console.Write(message + ": ");
+        if (!(int.TryParse(Console.ReadLine(), out number)))
+        {
+            System.Console.WriteLine("Введено некорректное число, попробуйте еще раз.");
+        }
+        else break;
+    }
+    return number;
+}
+
+int Sum(int M, int N)
+{
+    if (M == N)
+        return N;
+    return M + Sum(M + 1, N);
+}
+
+int m = InputNumber("Введите число M");
+int n = InputNumber("Введите число N");
+System.Console.WriteLine($"Сумма чисел от M до N = {Sum(m,n)}");
